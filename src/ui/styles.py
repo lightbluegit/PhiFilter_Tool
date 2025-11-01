52b2754e7830c1c0b6c9eb4055ecb318189c0440
kafuyuno <198796339@qq.com>
2025-11-02 00:58:15 +0800

    更新文件结构

---
+++ src/ui/styles.py
@@ -0,0 +1,159 @@
+from PyQt5.QtGui import QFontDatabase
+from utils.consts import CHI_FONT1_PATH
+
+# ---------------默认样式表获取---------------
+
+FONT_FAMILY = {}
+font_id = QFontDatabase.addApplicationFont(CHI_FONT1_PATH)
+if font_id < 0:
+    FONT_FAMILY["chi"] = "仿宋"
+    print("中文字体加载失败！ 已切换为仿宋")
+else:
+    font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+    FONT_FAMILY["chi"] = font_family
+
+
+def get_combobox_style(
+    font_size: str = 23,
+    font_family: str = FONT_FAMILY["chi"],
+    max_width: str = 360,
+    min_width: str = 360,
+    min_height: str = 50,
+    max_height: str = 50,
+    border_radius: str = 7,
+    background_color: tuple[str, str, str, str] = (255, 255, 255, 0),
+):
+    style = "ComboBox {\n"
+    r, g, b, a = background_color
+    style += f"""
+    font-size: {font_size}px;
+    font-family: "{font_family}";
+    max-width: {max_width}px;
+    min-width: {min_width}px;
+    min-height: {min_height}px;
+    max-height: {max_height}px;
+    border: 2px solid black;
+    border-radius: {border_radius}px;
+    background-color: rgba({r},{g},{b},{a});
+    text-align: left;
+    padding-left: 5px;
+    """
+    style += "}"
+    return style
+
+
+def get_button_style(
+    max_width: str = 360,
+    min_width: str = 360,
+    min_height: str = 50,
+    max_height: str = 50,
+    font_family: str = FONT_FAMILY["chi"],
+    font_size: str = 34,
+    border_radius: str = 7,
+    background_color: tuple[str, str, str, str] = (0, 159, 170, 1),
+    color: str = "white",
+):
+    style = "PushButton {\n "
+    r, g, b, a = background_color
+    contain = f"""
+    font-size: {font_size}px;
+    font-family: "{font_family}";
+    max-width: {max_width}px;
+    min-width: {min_width}px;
+    min-height: {min_height}px;
+    max-height: {max_height}px;
+    border-radius: {border_radius}px;
+    background-color: rgba({r},{g},{b},{a});
+    color: {color};
+    """
+    style += (
+        contain
+        + """
+        }
+    PushButton:hover {
+        background-color: #00CDCD;
+    }
+    PushButton:pressed {
+        background-color: #00FFFF;
+    }"""
+    )
+    return style
+
+
+def get_label_style(
+    font_size: str = 26,
+    max_width: str = 0,
+    min_width: str = 0,
+    min_height: str = 0,
+    max_height: str = 0,
+    font_family: str = FONT_FAMILY["chi"],
+    font_color: tuple[int, int, int, int] = (0, 0, 0, 1),
+    background_color: tuple[int, int, int, int] = (255, 255, 255, 0),
+    other: str = None,
+):
+    style = """QLabel {\n"""
+    r, g, b, a = background_color
+    fr, fg, fb, fa = font_color
+    style += f"""
+    font-size: {font_size}px;
+    font-family: '{font_family}';
+    color: rgba({fr},{fg},{fb},{fa});
+    background-color: rgba({r},{g},{b},{a});
+    """
+
+    if max_width > 0:
+        style += f"max-width: {max_width}px;\n"
+    if min_width > 0:
+        style += f"min-width: {min_width}px;\n"
+    if min_height > 0:
+        style += f"min-height: {min_height}px;\n"
+    if max_height > 0:
+        style += f"max-height: {max_height}px;\n"
+
+    style += (other if other is not None else "") + "}"
+    return style
+
+
+def get_input_box_style(
+    max_width: str = 300,
+    min_width: str = 300,
+    min_height: str = 60,
+    max_height: str = 60,
+    font_family: str = FONT_FAMILY["chi"],
+    font_size: str = 28,
+    border_radius: str = 7,
+    background_color: tuple[str, str, str, str] = (255, 248, 220, 1),
+):
+    style = "LineEdit {\n "
+    r, g, b, a = background_color
+    style += f"""
+    font-size: {font_size}px;
+    font-family: "{font_family}";
+    max-width: {max_width}px;
+    min-width: {min_width}px;
+    min-height: {min_height}px;
+    max-height: {max_height}px;
+    border-radius: {border_radius}px;
+    background-color: rgba({r},{g},{b},{a});
+    """
+    style += "}"
+    return style
+
+
+def get_switch_button_style(
+    font_family: str = FONT_FAMILY["chi"],
+    max_width: str = 210,
+    min_width: str = 210,
+    min_height: str = 30,
+    max_height: str = 30,
+):
+    style = "SwitchButton {\n"
+    style += f"""
+    font-family: "{font_family}";
+    max-width: {max_width}px;
+    min-width: {min_width}px;
+    min-height: {min_height}px;
+    max-height: {max_height}px;
+    """
+    style += "}"
+    return style
---
+++ src/utils/base.py
@@ -0,0 +1,95 @@
+from pathlib import Path
+import os
+import sys
+from enum import Enum
+
+debug: bool = False
+debug: bool = True
+
+
+def appdata_path(relative_path="", create_file=True):
+    """
+    获取可写的用户数据目录路径
+
+    Args:
+        relative_path: 相对路径，空字符串返回文件夹路径
+        create_file: 是否创建文件（当relative_path不为空时）
+    """
+    if debug:
+        base_path = Path.cwd()
+    else:
+        if sys.platform == "win32":
+            base = Path(os.environ["APPDATA"])
+
+            base_path = base / "PhiFilterTool"
+
+    # 确保基础目录存在
+    base_path.mkdir(parents=True, exist_ok=True)
+
+    if not relative_path:
+        # 返回文件夹路径
+        return str(base_path)
+
+    # 处理文件路径
+    file_path = base_path / relative_path
+
+    if create_file:
+        # 确保父目录存在
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        if not file_path.exists():
+            file_path.touch()  # 创建空文件
+            print(f"创建文件: {file_path}")
+
+    return str(file_path)
+
+
+def resource_path(relative_path):
+    """
+    获取打包后资源的正确路径
+    用法: resource_path("images/avatar/1.png")
+    """
+    if debug:
+        base_path = os.path.abspath(".")
+    else:
+        # PyInstaller 会创建临时文件夹，并把路径存入 sys._MEIPASS
+        base_path = sys._MEIPASS
+    return os.path.join(base_path, relative_path)
+
+
+LOG_PATH = "PhiFilterTool_log.txt"  # 日志文件
+def log_write(text: str):
+    if not isinstance(text, str):  # 防止入参不是str
+        return
+    with open(appdata_path(LOG_PATH), "a+", encoding="utf-8") as f:
+        f.write(text + "\n")
+
+
+class score_level_type(Enum):
+    F = "F"
+    C = "C"
+    B = "B"
+    A = "A"
+    S = "S"
+    V = "V"
+    VFC = "蓝V"
+    phi = "phi"
+
+
+# 获取评级
+def get_score_level(score: int, is_fc: bool = False) -> score_level_type:
+    if score == 1000000:
+        return score_level_type.phi
+    elif is_fc:
+        return score_level_type.VFC
+    elif score >= 960000:
+        return score_level_type.V
+    elif score >= 920000:
+        return score_level_type.S
+    elif score >= 880000:
+        return score_level_type.A
+    elif score >= 820000:
+        return score_level_type.B
+    elif score >= 600000:
+        return score_level_type.C
+    else:
+        return score_level_type.F
